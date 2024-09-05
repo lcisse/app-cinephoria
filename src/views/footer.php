5234fbd00e5a245b40a8da1dcd7b471c61798f4d
lcisse <sotiba67@gmail.com>
2024-09-05 16:07:26 +0200

    layout creation + buton on card hover

---
+++ src/views/footer.php
@@ -0,0 +1,22 @@
+<footer class="pt-4 pb-4">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-4 mb-3">
+                  <img src="public/assets/images/logo.png" alt="Logo de Cinéphoria" class="mb-2">
+                  <p>Le cinéma responsable qui vous apporte les meilleurs films tout en prenant soin de notre planète.</p>
+              </div>
+              <div class="col-md-4 mb-3">
+                  <h5>Horaires</h5>
+                  <ul class="list-unstyled">
+                      <li>Lundi-Vendredi: 9h - 22h</li>
+                      <li>Samedi-Dimanche: 10h - 23h</li>
+                  </ul>
+              </div>
+              <div class="col-md-4 mb-3">
+                  <h5>Contact</h5>
+                  <p><strong>Adresse:</strong> 123 Rue Exemple, 75001 Paris</p>
+                  <p><strong>Téléphone:</strong> +33 1 23 45 67 89</p>
+              </div>
+          </div>
+      </div>
+</footer>
---
+++ src/views/navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary" id="navbarTop">
+        <div class="container-fluid justify-content-between" id="container-nav"> 
+          <a class="navbar-brand" href="#"><img src="public/assets/images/logo.png" alt="Logo de Cinéphoria" class="mb-2"></a> 
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0"> <!-- Changement de me-auto à ms-auto pour aligner à droite -->
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">FILM</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  RESERVATION
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Nantes</a></li>
+                  <li><a class="dropdown-item" href="#">Bordeaux</a></li>
+                  <li><a class="dropdown-item" href="#">Paris</a></li>
+                  <li><a class="dropdown-item" href="#">Toulouse</a></li>
+                  <li><a class="dropdown-item" href="#">Lille</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Charleroi</a></li>
+                  <li><a class="dropdown-item" href="#">Liège</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">MON COMPTE</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
